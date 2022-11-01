22df5b97b6776ac09d9526c7b48dd101311902ce
dankeboy36 <dankeboy36@gmail.com>
2022-11-01 14:02:42 +0100

    feat: logging
    Signed-off-by: dankeboy36 <dankeboy36@gmail.com>

--- README.md
+++ README.md
@@ -11,42 +11,60 @@ npm i ardunno-cli-gen --save
 ## Usage
 
 ```
-ardunno-cli generate [options] <src>
+Usage: ardunno-cli generate [options] <src>
 
 Generates TS/JS API for the Arduino CLI
 
 Arguments:
-  src          The source of the proto files to generate from. The input source can be a path
-               to the folder which contains the proto files. The source can be a valid semver.
-               Then, the proto files will be downloaded from the Arduino CLI's GitHub release.
-               It can be a GitHub commit in the following format
-               `(?<owner>)/(?<repo>)(#(?<commit>))?`. Then, the proto files will be cloned and
-               checked out from GitHub.
+  src                 The source of the proto files to generate from.
+                      The input source can be a path to the folder
+                      which contains the proto files. The source can be
+                      a valid semver. Then, the proto files will be
+                      downloaded from the Arduino CLI's GitHub release.
+                      It can be a GitHub commit in the following format
+                      `(?<owner>)/(?<repo>)(#(?<commit>))?`. Then, the
+                      proto files will be cloned and checked out from
+                      GitHub.
 
 Options:
-  -o, --out    Specify an output folder for all emitted files.
-  -f, --force  Override previously emitted files in the output location.
-  -h, --help   display help for command
+  -o, --out <string>  Specify an output folder for all emitted files.
+  -f, --force         Override previously emitted files in the output
+                      location.
+  -h, --help          display help for command
 ```
 
 Examples:
 
 ```sh
 # generates from local proto files
-./ardunno-cli generate ./path/to/rpc -o ./src-gen
+npx ardunno-cli generate ./path/to/rpc -o ./src-gen
 ```
 
 ```sh
 # generates from a valid semver
-./ardunno-cli generate 0.28.0 -o ./src-gen
+npx ardunno-cli generate 0.28.0 -o ./src-gen
 ```
 
 ```sh
 # generates from the HEAD of the default branch
-./ardunno-cli generate arduino/arduino-cli -o ./src-gen
+npx ardunno-cli generate arduino/arduino-cli -o ./src-gen
 ```
 
 ```sh
 # generates from a specific commit
-./ardunno-cli generate arduino/arduino-cli#5a4ffe0 -o ./src-gen
+npx ardunno-cli generate arduino/arduino-cli#5a4ffe0 -o ./src-gen
+```
+
+```sh
+# generates from a specific commit of a fork
+npx ardunno-cli generate cmaglie/arduino-cli#alternate-homedir -o ./src-gen
+```
+
+### API
+
+```js
+const { generate } = require('ardunno-cli-gen');
+(async () => {
+    await generate({ src: '0.28.0', out: './src-gen', force: true });
+})();
 ```
--- src/generate.ts
+++ src/generate.ts
@@ -1,11 +1,14 @@
+import { debug } from 'debug';
 import execa from 'execa';
 import { promises as fs } from 'fs';
 import globby from 'globby';
 import isValidPath from 'is-valid-path';
 import { isAbsolute, join } from 'path';
+import rimraf from 'rimraf';
 import { gt, SemVer, valid } from 'semver';
 import { dir } from 'tmp-promise';
-import rimraf from 'rimraf';
+
+const log = debug('ardunno-cli-gen');
 
 export interface Options {
     readonly src: string;
@@ -15,16 +18,26 @@ export interface Options {
 
 export default async function (options: Options): Promise<void> {
     const { src, out, force } = options;
+    log('generating with %j', options);
     const [outExists, protos] = await Promise.all([accessibleFolder(out), globProtos(src)]);
     if (!force && outExists) {
         throw new Error(`${out} already exists. Use '--force' to override output`);
     }
-
+    if (!outExists) {
+        try {
+            await fs.mkdir(out, { recursive: true });
+        } catch (err) {
+            log('failed to create --out %O', err);
+            throw new Error(`Failed to create --out: ${err}`);
+        }
+    }
     if (protos) {
+        log('found protos %j', protos);
         return generate(src, protos, out);
     }
     let semver = parseSemver(src);
     if (typeof semver === 'string') {
+        log('found semver %s', protos);
         console.warn(
             'Downloading the proto files from the GitHub release is not yet available. Falling back to Git clone. See https://github.com/arduino/arduino-cli/pull/1931.'
         );
@@ -32,12 +45,14 @@ export default async function (options: Options): Promise<void> {
             ...arduinoGitHub,
             commit: semver,
         };
+        log('semver is not supported yet. falling back to GitHub %j', semver);
     }
     const gh = semver || parseGitHub(src);
     if (gh) {
         const { dispose, checkoutSrc } = await checkout(gh);
         try {
             const clonedProtos = await globProtos(checkoutSrc);
+            log('cloned protos %j', clonedProtos);
             if (!clonedProtos) {
                 throw new Error(`Failed to glob in ${checkoutSrc}`);
             }
@@ -48,21 +63,13 @@ export default async function (options: Options): Promise<void> {
     }
     throw new Error(`Invalid <src>: ${src}`);
 }
+
 interface Plugin {
     readonly name: string;
     readonly options: Record<string, string | string[] | boolean | boolean[]>;
     readonly path: string;
 }
-function createArgs(plugin: Plugin, src: string, out: string): string[] {
-    const { name, options, path } = plugin;
-    const opt = Object.entries(options)
-        .reduce(
-            (acc, [key, value]) => acc.concat((Array.isArray(value) ? value : [value]).map((v) => `${key}=${v}`)),
-            [] as string[]
-        )
-        .join(',');
-    return [`--plugin=${path}`, `--proto_path=${src}`, `--${name}_opt=${opt}`, `--${name}_out=${out}`];
-}
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const TsProto: Plugin = {
     name: 'ts_proto',
@@ -76,31 +83,47 @@ const TsProto: Plugin = {
     },
 };
 
+function createArgs(plugin: Plugin, src: string, out: string): string[] {
+    const { name, options, path } = plugin;
+    const opt = Object.entries(options)
+        .reduce(
+            (acc, [key, value]) => acc.concat((Array.isArray(value) ? value : [value]).map((v) => `${key}=${v}`)),
+            [] as string[]
+        )
+        .join(',');
+    return [`--plugin=${path}`, `--proto_path=${src}`, `--${name}_opt=${opt}`, `--${name}_out=${out}`];
+}
+
 async function generate(src: string, protos: string[], out: string): Promise<void> {
     const protoc = require('protoc/protoc');
     const args = [...createArgs(TsProto, src, out), ...protos];
+    log('executing %s with args %j', protoc, args);
     await execa(protoc, args);
 }
 
 async function globProtos(cwd: string): Promise<string[] | undefined> {
-    if (!isValidPath(cwd)) {
-        return undefined;
-    }
+    log('glob %s', cwd);
     if (!(await accessibleFolder(cwd))) {
+        log('glob invalid path %s', cwd);
         return undefined;
     }
     return globby('**/*.proto', { cwd });
 }
 
 async function accessibleFolder(maybePath: string): Promise<boolean> {
+    log('accessible %s', maybePath);
     if (!isValidPath(maybePath)) {
+        log('accessible invalid path %s', maybePath);
         return false;
     }
     const path = isAbsolute(maybePath) ? maybePath : join(process.cwd(), maybePath);
     try {
         const stat = await fs.stat(path);
-        return stat.isDirectory();
+        const dir = stat.isDirectory();
+        log('is dir %s %d', path, dir);
+        return dir;
     } catch {
+        log('stat failed %s', path);
         return false;
     }
 }
@@ -109,48 +132,60 @@ async function accessibleFolder(maybePath: string): Promise<boolean> {
 // owner name can contain only hyphens
 // repo name can contain dots and underscores
 // commit can be a branch, a hash, tag, etc, anything that git can `checkout` TODO: use https://git-scm.com/docs/git-check-ref-format?
-const gh = /^(?<owner>([0-9a-zA-Z-]+))\/(?<repo>([0-9a-zA-Z-_\.]+))(#(?<commit>([^\s]+)))?$/;
+const ghPattern = /^(?<owner>([0-9a-zA-Z-]+))\/(?<repo>([0-9a-zA-Z-_\.]+))(#(?<commit>([^\s]+)))?$/;
 const arduinoGitHub: GitHub = {
     owner: 'arduino',
     repo: 'arduino-cli',
 };
 
-export interface GitHub {
+interface GitHub {
     readonly owner: string;
     readonly repo: string;
     readonly commit?: string | undefined;
 }
+
+// (non-API)
 export function parseGitHub(src: string): GitHub | undefined {
-    const match: RegExpGroups<['owner', 'repo', 'commit']> = src.match(gh);
+    const match: RegExpGroups<['owner', 'repo', 'commit']> = src.match(ghPattern);
     if (match && match.groups) {
         const {
             groups: { owner, repo, commit },
         } = match;
-        return {
+        const gh = {
             owner,
             repo,
             ...(commit && { commit }),
         };
+        log('match GitHub %s, %j', src, gh);
+        return gh;
     }
+    log('no match GitHub %s', src);
     return undefined;
 }
 
 async function checkout(gh: GitHub): Promise<{ checkoutSrc: string; dispose: () => Promise<void> }> {
     const { owner, repo, commit = 'HEAD' } = gh;
     const { path } = await dir({ prefix: repo });
-    await execa('git', ['clone', `https://github.com/${owner}/${repo}.git`, path]);
-    await execa('git', ['-C', path, 'fetch', '--all', '--tags']);
-    await execa('git', ['-C', path, 'checkout', commit]);
-    return { checkoutSrc: join(path, 'rpc'), dispose: () => rm(path) };
+    log('checkout %j', gh);
+    log('checkout dir %s', path);
+    const url = `https://github.com/${owner}/${repo}.git`;
+    await execa('git', ['clone', url, path], { stdio: 'pipe' });
+    log('cloned from %s to %s', url, path);
+    await execa('git', ['-C', path, 'fetch', '--all', '--tags'], { stdio: 'pipe' });
+    log('fetched all from %s', url);
+    await execa('git', ['-C', path, 'checkout', commit], { stdio: 'pipe' });
+    log('checked out %s from %s', commit, url);
+    return { checkoutSrc: join(path, 'rpc'), dispose: () => rmrf(path) };
 }
 
-async function rm(path: string): Promise<void> {
+async function rmrf(path: string): Promise<void> {
     return new Promise<void>((resolve, reject) => rimraf(path, (error) => (error ? reject(error) : resolve())));
 }
 
 /**
  * The `.proto` files are not part of the Arduino CLI release until before version 0.29.0+ ([`arduino/arduino-cli#1931`](https://github.com/arduino/arduino-cli/pull/1931)). It provides the GitHub ref instead.
  */
+// (non-API)
 export function parseSemver(src: string): string | GitHub | undefined {
     if (!valid(src)) {
         return undefined;
