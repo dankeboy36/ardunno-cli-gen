19058006cc0267640ae23b6c92eda60dc06609d7
dankeboy36 <dankeboy36@gmail.com>
2022-11-19 18:19:44 +0100

    feat: generate API from GitHub release assets
    Signed-off-by: dankeboy36 <dankeboy36@gmail.com>

--- src/__tests__/generate.spec.ts
+++ src/__tests__/generate.spec.ts
@@ -5,7 +5,10 @@ import { parseGitHub, parseSemver } from '../generate';
 describe('generate', () => {
     describe('parseGitHub', () => {
         it('should parse valid', () => {
-            assert.deepEqual(parseGitHub('arduino/arduino-cli'), { owner: 'arduino', repo: 'arduino-cli' });
+            assert.deepEqual(parseGitHub('arduino/arduino-cli'), {
+                owner: 'arduino',
+                repo: 'arduino-cli',
+            });
         });
         it('should parse valid with commit', () => {
             assert.deepEqual(parseGitHub('arduino/arduino-cli#5a4ffe0'), {
@@ -22,20 +25,29 @@ describe('generate', () => {
             '/owner/repo',
             'owner/repo/',
             'owner/repo#one two',
-        ].forEach((src) => it(`should not parse '${src}'`, () => assert.equal(parseGitHub(src), undefined)));
+        ].forEach((src) =>
+            it(`should not parse '${src}'`, () =>
+                assert.equal(parseGitHub(src), undefined))
+        );
     });
     describe('parseSemver', () => {
-        it('should parse valid', () => assert.equal(parseSemver('0.30.0'), '0.30.0'));
-        it('should parse valid with rc', () => assert.equal(parseSemver('0.30.0-rc1'), '0.30.0-rc1'));
-        it("should parse valid with 'v' prefix", () => assert.equal(parseSemver('v0.29.1'), '0.29.1'));
-        it("should parse to GitHub ref when version is not greater than '0.29.0'", () =>
-            assert.deepEqual(parseSemver('0.29.0'), {
+        it('should parse valid', () =>
+            assert.equal(parseSemver('0.30.0'), '0.30.0'));
+        it('should parse valid with rc', () =>
+            assert.equal(parseSemver('0.30.0-rc1'), '0.30.0-rc1'));
+        it("should parse valid with 'v' prefix", () =>
+            assert.equal(parseSemver('v0.29.1'), '0.29.1'));
+        it("should parse valid semver '>=0.29.0' as a semver [arduino/arduino-cli#1931]", () =>
+            assert.equal(parseSemver('0.29.0'), '0.29.0'));
+        it("should parse to GitHub ref when version is not greater than '0.28.0'", () =>
+            assert.deepEqual(parseSemver('0.28.0'), {
                 owner: 'arduino',
                 repo: 'arduino-cli',
-                commit: '0.29.0',
+                commit: '0.28.0',
             }));
         ['a', '0', '0.30', '0.30.', '0.30.0.'].forEach((src) =>
-            it(`should not parse '${src}'`, () => assert.equal(parseSemver(src), undefined))
+            it(`should not parse '${src}'`, () =>
+                assert.equal(parseSemver(src), undefined))
         );
     });
 });
--- src/cli.ts
+++ src/cli.ts
@@ -14,8 +14,14 @@ program
         '<src>',
         "The source of the proto files to generate from. The input source can be a path to the folder which contains the proto files. The source can be a valid semver. Then, the proto files will be downloaded from the Arduino CLI's GitHub release. It can be a GitHub commit in the following format `(?<owner>)/(?<repo>)(#(?<commit>))?`. Then, the proto files will be cloned and checked out from GitHub."
     )
-    .requiredOption('-o, --out <string>', 'Specify an output folder for all emitted files.')
-    .option('-f, --force', 'Override previously emitted files in the output location.')
+    .requiredOption(
+        '-o, --out <string>',
+        'Specify an output folder for all emitted files.'
+    )
+    .option(
+        '-f, --force',
+        'Override previously emitted files in the output location.'
+    )
     .action(async (arg, options) => {
         const src = String(arg);
         const force = Boolean(options.force);
